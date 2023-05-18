1a2af1f4769fd32b6df7d77b56e699f638b86787
Kenny Tang <116704394+kennyta0204@users.noreply.github.com>
2023-05-17 20:08:23 -0700

    add working update book

--- update_2023-05-17.sql
+++ update_2023-05-17.sql
@@ -1,2 +1,14 @@
 INSERT INTO car_make (make, country) VALUES ('PONTIAC', 'US');
 
+INSERT INTO car_name (name, make_id) VALUES ('gto', 30);
+INSERT INTO car_name (name, make_id) VALUES ('400z', 21);
+INSERT INTO car_models (name_id, edition, min_year, max_year, hp, torque, body, door, cylinder, displacement, aspiration, transmission, drive) 
+        VALUES ('35','turbo (fs)',2012,2018,201,195,'hatch',3,4,1.6,'turbo','both','fwd');
+INSERT INTO car_models (name_id, edition, min_year, max_year, hp, torque, body, door, cylinder, displacement, aspiration, transmission, drive) 
+        VALUES ('35','n (js)',2019,2022,275,260,'hatch',3,4,2.0,'turbo','both','fwd');
+INSERT INTO car_models (name_id, edition, min_year, max_year, hp, torque, body, door, cylinder, displacement, aspiration, transmission, drive) 
+        VALUES ('80','5th gen',2004,2004,350,365,'coupe',2,8,5.7,'na','both','rwd');
+INSERT INTO car_models (name_id, edition, min_year, max_year, hp, torque, body, door, cylinder, displacement, aspiration, transmission, drive) 
+        VALUES ('80','5th gen',2005,2006,400,400,'coupe',2,8,6.0,'na','both','rwd');
+INSERT INTO car_models (name_id, edition, min_year, max_year, hp, torque, body, door, cylinder, displacement, aspiration, transmission, drive) 
+        VALUES ('81','base',2022,9999,400,350,'coupe',2,6,3.0,'turbo','both','rwd');
