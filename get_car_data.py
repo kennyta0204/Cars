eceb5b5ffeff2a235eaaf1861b304b0a7527e846
Kenny Tang <kennyta@umich.edu>
2023-06-01 01:03:32 -0700

    recommender model

--- get_car_data.py
+++ get_car_data.py
@@ -18,8 +18,20 @@ def get_data():
 
     with engine.connect() as connn:
         query = connn.execute(text(sql))
+    
+    df = pd.DataFrame(query.fetchall())
 
-    return pd.DataFrame(query.fetchall())
+    df['max_year'] = [y if y <9999 else 2023 for y in df['max_year']]
+    df['turbo'] = [1 if a == 'turbo' else 0 for a in df.aspiration]
+    df['super'] = [1 if a == 'supercharged' else 0 for a in df.aspiration]
+    df['two_door'] = [1 if door == 2 else 0 for door in df.door]
+    df['man'] = [1 if t == 'man' or t=='both' else 0 for t in df.transmission]
+    df['auto'] = [1 if t == 'auto' or t=='both' else 0 for t in df.transmission]
+    df['fwd'] = [1 if d == 'fwd' else 0 for d in df.drive]
+    df['rwd'] = [1 if d == 'rwd' else 0 for d in df.drive]
+    df['awd'] = [1 if d == 'awd' or d == '4wd' else 0 for d in df.drive]
+
+    return df
 
 if __name__ == '__main__':
     import argparse
@@ -29,6 +41,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     save_path = "data/" + args.output
 
-    get_data().to_csv(save_path)
+    get_data().to_csv(save_path, index=False)
 
 
---
+++ train.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy.sql import text
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import pickle
+
+def mydist(x, y):
+    x_perf = x[:2]
+    y_perf = y[:2]
+    x_feats = x[2:]
+    y_feats = y[2:]
+    
+    perf = np.sqrt(np.sum((x_perf-y_perf)**2))
+    feats = (len(x_feats) - np.sum((x_feats-y_feats)**2)) / len(x_feats)
+    dist = perf/feats
+    
+    return dist
+
+def train(data):
+    df = pd.read_csv(data, index_col=False)
+    X = df[['hp','torque','turbo', 'super', 'two_door','man','auto','fwd','rwd','awd']].to_numpy()
+
+    model_knn =NearestNeighbors(n_neighbors= 5, algorithm= 'ball_tree', metric= mydist)
+    model_knn.fit(X)
+
+    return model_knn
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help= 'Car data file (csv)')
+    args =parser.parse_args()
+
+    with open('model_knn.pkl', 'wb') as handle:
+        pickle.dump(train(args.input), handle)
